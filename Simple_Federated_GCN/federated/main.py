3ed870d452f2791cc177c2882dbb5f5198bbbfe4
Nikolaos Pavlidis <nikopavl4@gmail.com>
2022-02-14 17:11:56 +0200

    SAGE added

--- Simple_Federated_GCN/federated/main.py
+++ Simple_Federated_GCN/federated/main.py
@@ -29,9 +29,19 @@ train_loader2 = DataLoader(train_dataset1, batch_size=150, shuffle=True)
 train_loader3 = DataLoader(train_dataset3, batch_size=150, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=150, shuffle=False)
 
-model1 = GCN(hidden_channels=16,dataset=dataset)
-model2 = GCN(hidden_channels=16,dataset=dataset)
-model3 = GCN(hidden_channels=16,dataset=dataset)
+
+if sys.argv[1].lower() == "gcn":
+    model1 = GCN(hidden_channels=16,dataset=dataset)
+    model2 = GCN(hidden_channels=16,dataset=dataset)
+    model3 = GCN(hidden_channels=16,dataset=dataset)
+elif sys.argv[1].lower() == "graphsage":
+    model1 = SAGE(hidden_channels=16,dataset=dataset)
+    model2 = SAGE(hidden_channels=16,dataset=dataset)
+    model3 = SAGE(hidden_channels=16,dataset=dataset)
+else:
+    print("Model does not exist! Please select GCN or GraphSAGE")
+    exit()
+
 
 
 for epoch in range(1, 70):
@@ -56,13 +66,16 @@ for epoch in range(1, 70):
     #print("Client3:")
     #print(f'Epoch: {epoch:03d}, Train Acc: {train_acc_3:.4f}, Test Acc: {test_acc_3:.4f}')
 
-if sys.argv[1].casefold() == "GCN":
+if sys.argv[1].lower() == "gcn":
     server_model = GCN(hidden_channels=16,dataset=dataset)
-elif sys.argv[1].casefold() == "GraphSAGE":
-    server_model = GCN(hidden_channels=16,dataset=dataset)
+    print("Model: GCN")
+elif sys.argv[1].lower() == "graphsage":
+    server_model = SAGE(hidden_channels=16,dataset=dataset)
+    print("Model: GraphSAGE")
 else:
     print("Model does not exist! Please select GCN or GraphSAGE")
-    
+    exit()
+
 server_accuracy_per_round = []
 
 for round_id in range(1,11):
