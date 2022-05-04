e55758094c743873f594aa73b9d3ec05995c3747
Nikolaos Pavlidis <nikopavl4@gmail.com>
2022-05-04 21:36:11 +0300

    .

--- Simple_Federated_GCN/Graph Classification/federated/server.py
+++ Simple_Federated_GCN/Graph Classification/federated/server.py
@@ -4,9 +4,9 @@ from random import sample
 random.seed(12345)
 
 def perform_federated_round(server_model,Client_list,round_id,test_loader, args):
-    #Client_list = sample(Client_list, args.parameterC)
-    for param_tensor in Client_list[0].model.state_dict():
-        avg = (sum(c.model.state_dict()[param_tensor] for c in Client_list))/len(Client_list)
+    Client_list2 = sample(Client_list, args.parameterC)
+    for param_tensor in Client_list2[0].model.state_dict():
+        avg = (sum(c.model.state_dict()[param_tensor] for c in Client_list2))/len(Client_list2)
         server_model.state_dict()[param_tensor].copy_(avg)
         for cl in Client_list:
             cl.model.state_dict()[param_tensor].copy_(avg)
